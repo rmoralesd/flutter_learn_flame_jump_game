8521c485073f9ed0ce58df9cffdfff7957df8723
Ricardo Morales <rmorales8303@gmail.com>
2023-01-04 08:17:11 -0500

    Adición de la barra giratoria

--- lib/lesson_02/game_lesson_02.dart
+++ lib/lesson_02/game_lesson_02.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_learn_flame_jump_game/lesson_01/objects/ball.dart';
+import 'package:flutter_learn_flame_jump_game/lesson_02/objects/box_kinematic.dart';
 import 'package:flutter_learn_flame_jump_game/lesson_02/objects/floor_static.dart';
 import 'package:flutter_learn_flame_jump_game/my_game.dart';
 
@@ -7,5 +8,6 @@ class GameLesson02 extends MyGame {
   Future<void> onLoad() async {
     super.onLoad();
     add(FloorStatic());
+    add(BoxKinematic());
   }
 }
